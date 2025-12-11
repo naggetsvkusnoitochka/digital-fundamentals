b66078aea12dc3f6ade690edabe0e6228fc108c5
Комракова Анастасия Станиславовна <nastya.stanisl@gmail.com>
2025-12-11 20:21:22 +0300

    Update analyser4.sh

--- scripts/analyser4.sh
+++ scripts/analyser4.sh
@@ -1,1 +1,29 @@
-
+#!/bin/bash
+min=228
+infomin=""
+max=0
+infomax=""
+for i in $(ls $1)
+do
+    for string in $(cat $1$i)
+    do
+        age=$(echo $string | cut -d ":" -f2)
+        if [[ $age -gt $max ]]
+        then 
+            max=$age
+            name=$(echo $string | cut -d ":" -f1)
+            email=$(echo $string |  cut -d ":" -f3)
+            infomax=$(echo -e "Имя: $name, возраст: $age, почта: $email")
+        fi
+        if [[ $age -lt $min ]]
+        then
+            min=$age
+            name=$(echo $string | cut -d ":" -f1)
+            email=$(echo $string | cut -d ":"  -f3)
+            infomin=$(echo -e "Имя: $name, возраст: $age, почта: $email")
+        fi
+    done
+done
+            
+echo $infomin
+echo $infomax
